05c09dad97c7dc8a67749d490dd45a780e33c07f
Neerav Karani <nkarani@ethz.ch>
2020-03-10 14:45:58 +0100

    Added 'first_run' option
    This enables one time functions such as bias correction.

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 import load_datasets
 
 # ================================
-# set anatomy here.
+# set anatomy here
 # ================================
 # 'brain' / 'cardiac' / 'prostate'
-ANATOMY = 'cardiac'
+# ================================
+ANATOMY = 'prostate'
 
 # ================================
 # set dataset here.
@@ -15,14 +16,19 @@ ANATOMY = 'cardiac'
 # for brain: 'HCP_T1' / 'HCP_T2' / 'ABIDE_caltech' / 'ABIDE_stanford'
 # for cardiac: 'ACDC' / 'RVSC'
 # for prostate: 'NCI' / 'PIRAD_ERC'
-DATASET = 'RVSC' 
+# ================================
+DATASET = 'NCI' 
 
 # ================================
 # set train / test / validation here
 # ================================
 # 'train' / 'test' / 'validation'
-TRAIN_TEST_VALIDATION = 'validation'
+TRAIN_TEST_VALIDATION = 'train'
 
+# ================================
+# read images and segmentation labels
+# ================================
 images, labels = load_datasets.load_dataset(anatomy = ANATOMY,
                                             dataset = DATASET,
-                                            train_test_validation = TRAIN_TEST_VALIDATION)
+                                            train_test_validation = TRAIN_TEST_VALIDATION,
+                                            first_run = False)  # <-- SET TO TRUE FOR THE FIRST RUN (enables preliminary preprocessing e.g. bias field correction)
